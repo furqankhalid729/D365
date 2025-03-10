bc00614a765f779767792414ce6a603ac4779da0
furqank729 <58474453+furqankhalid729@users.noreply.github.com>
2025-03-11 02:54:58 +0500

    Inventory dynamic

--- app/Http/Controllers/InventoryController.php
+++ app/Http/Controllers/InventoryController.php
@@ -12,10 +12,10 @@ class InventoryController extends Controller
 {
     public function store(Request $request)
     {
-        if (!$request->has('sku') || empty($request->sku)) {
+        if (!$request->has('sku') || empty($request->sku) || !$request->has('quantity') || empty($request->quantity)) {
             return response()->json([
                 'success' => false,
-                'message' => 'SKU is required'
+                'message' => 'SKU/Quantity is required'
             ], 400);
         }
 
@@ -24,10 +24,12 @@ class InventoryController extends Controller
             $client = new Rest(env("STORE_URL"), env("SHOPIFY_ACCESS_TOKEN"));
             $variantInventoryID = $this->getInventoryItemId($sku);
             $cleanedInventoryItemId = str_replace('gid://shopify/InventoryItem/', '', $variantInventoryID);
+            $quantity = (int) $request->quantity;
+            $formattedQuantity = ($quantity < 0 ? '-' : '+') . abs($quantity);
             $body = [
                 'inventory_item_id' => (int) $cleanedInventoryItemId,
                 'location_id' => env("LOCATION_ID"),
-                'available_adjustment' => 5
+                'available_adjustment' => $formattedQuantity
             ];
             $response = $client->post(path: 'inventory_levels/adjust.json', body: $body);
             $responseBody = $response->getBody();
