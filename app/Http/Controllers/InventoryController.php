5732fd40449a51dcc99b3680035c156d46e8bb52
furqank729 <58474453+furqankhalid729@users.noreply.github.com>
2025-03-27 17:58:09 +0500

    Abs Inventory Value

--- app/Http/Controllers/InventoryController.php
+++ app/Http/Controllers/InventoryController.php
@@ -44,6 +44,39 @@ class InventoryController extends Controller
         }
     }
 
+    public function setAbsoluteValue(Request $request)
+    {
+        if (!$request->has('sku') || empty($request->sku) || !$request->has('quantity') || empty($request->quantity)) {
+            return response()->json([
+                'success' => false,
+                'message' => 'SKU/Quantity is required'
+            ], 400);
+        }
+        try {
+            $sku = $request->sku;
+            $client = new Rest(env("STORE_URL"), env("SHOPIFY_ACCESS_TOKEN"));
+            $variantInventoryID = $this->getInventoryItemId($sku);
+            $cleanedInventoryItemId = str_replace('gid://shopify/InventoryItem/', '', $variantInventoryID);
+            $quantity = (int) $request->quantity;
+            // $formattedQuantity = ($quantity < 0 ? '-' : '+') . abs($quantity);
+            $body = [
+                'inventory_item_id' => (int) $cleanedInventoryItemId,
+                'location_id' => env("LOCATION_ID"),
+                'available' => $quantity
+            ];
+            $response = $client->post(path: 'inventory_levels/set.json', body: $body);
+            $responseBody = $response->getBody();
+            $jsonResponseBody = json_decode($responseBody, true);
+            return response()->json([$jsonResponseBody, $body], 201);
+        } catch (\Exception $e) {
+            Log::error("Error fetching inventory item ID: " . $e->getMessage());
+            return response()->json([
+                'success' => false,
+                'message' => 'An error occurred while retrieving the inventory item ID.'
+            ], 500);
+        }
+    }
+
 
     private function getInventoryItemId($sku)
     {
--- routes/api.php
+++ routes/api.php
@@ -10,5 +10,6 @@ Route::post('/api/get-token', [TokenController::class, 'getToken']);
 Route::get('/api/product', [ProductController::class, 'index'])->middleware('custom.auth');
 Route::post('/api/product', [ProductController::class, 'store'])->middleware('custom.auth');
 Route::post('/api/inventory', [InventoryController::class, 'store'])->middleware('custom.auth');
+Route::post('/api/inventory/abs', [InventoryController::class, 'setAbsoluteValue'])->middleware('custom.auth');
 
 Route::post('/api/order/create', [OrderController::class, 'store']);//->middleware('verify.shopify.webhook');
